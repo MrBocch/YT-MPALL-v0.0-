98284acfbf6eeacdab5c726bf677c27d508ccb94
Jorge <126930965+MrBocch@users.noreply.github.com>
2024-01-07 16:55:05 -0600

    limpiar codigo

--- main.py
+++ main.py
@@ -2,22 +2,27 @@ import os
 import re
 from pathlib import Path
 from pytube import YouTube
+
 def setup():
     Path("downloads").mkdir(exist_ok=True)
+    
 def get_format_menu():
     print("====FORMATO_DE_AUDIO====")
     print("1) MP3")
     print("2) AAC")
     print("3) FLAC")
     return input("Elige una opción: ")
+    
 def get_video_format_menu():
     print("====FORMATO_DE_VIDEO====")
     print("1) MP4")
     print("2) AVI")
     print("3) MKV")
     return input("Elige una opción: ")
+    
 def get_link():
     return input("====LINK====\nCopia y pega la URL: ")
+    
 def download_audio(link, format_option):
     try:
         yt = YouTube(link)
@@ -32,17 +37,23 @@ def download_audio(link, format_option):
             print("Opción no válida.")
     except Exception as e:
         print(f"Error descargando {link}: {e}")
+        
 def download_video(link, format_option):
     try:
         yt = YouTube(link)
         print(f"Descargando {yt.title}")
-        if format_option == '1':            yt.streams.filter(file_extension='mp4').first().download(output_path="downloads")
-        elif format_option == '2':           yt.streams.filter(file_extension='avi').first().download(output_path="downloads")
-        elif format_option == '3':            yt.streams.filter(file_extension='mkv').first().download(output_path="downloads")
+        if format_option == '1': 
+            yt.streams.filter(file_extension='mp4').first().download(output_path="downloads")
+        elif format_option == '2':           
+            yt.streams.filter(file_extension='avi').first().download(output_path="downloads")
+        elif format_option == '3':            
+            yt.streams.filter(file_extension='mkv').first().download(output_path="downloads")
         else:
             print("Opción no válida.")
+    
     except Exception as e:
         print(f"Error descargando {link}: {e}")
+        
 def main():
     while True:
         print("====MENU_PRINCIPAL====")
@@ -50,18 +61,23 @@ def main():
         print("2) Descargar video")
         print("3) Salir")
         option = input("Elige una opción: ")
+        
         if option == '1':
             audio_format_option = get_format_menu()
             link = get_link()
             download_audio(link, audio_format_option)
+        
         elif option == '2':
             video_format_option = get_video_format_menu()
             link = get_link()
             download_video(link, video_format_option)
+        
         elif option == '3':
             break
+        
         else:
             print("Opción no válida.")
+            
 if __name__ == "__main__":
     setup()
     main()
